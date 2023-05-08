190715c85943c73c40e974115202480e9e07c1da
FaceLessHacker999 <131527154+FaceLessHacker999@users.noreply.github.com>
2023-05-08 15:19:54 +0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,45 @@
+
+#!/bin/bash
+#code_recorded_by_facelesshacker
+clear
+sleep 1
+echo ""
+echo ""
+echo "Funku| lolcat
+
+
+
+
+echo ""
+sleep 1
+clear
+
+pkg install php -y
+pkg install openssh -y
+pkg install unzip -y
+pkg install curl -y
+sleep 1
+clear
+
+echo "Loading==> " | lolcat
+sleep 1
+clear
+echo "Loading====> " | lolcat
+sleep 1
+clear
+echo "Loading=======> " | lolcat
+sleep 1
+clear
+echo " done " | lolcat
+sleep 1
+clear
+
+clear
+bash about.sh
+sleep 4
+echo ""
+echo ""
+echo " 
+echo ""
+sleep 10
+ ""
